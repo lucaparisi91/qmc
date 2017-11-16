6502cdb1d169976bcf63a28caa8c7fb2cb87e154
lucaparisi91 <luca.parisi91@mail.com>
2017-11-16 11:01:58 +0100

    Changed Config

--- config.sh
+++ config.sh
@@ -1,6 +1,8 @@
+echo "Generating Makefile"
 cat qmc.config src/MakefileTemplate > src/Makefile
 echo "Building Build Directory..."
 mkdir -p build
 mkdir -p build/observables
 mkdir -p build/particles
 mkdir -p build/qmcDriver
+echo "Done"
